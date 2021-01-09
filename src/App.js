26012e11830b60c10cf9deae28c163d1f53bfe26
KilHyun-Kim <rlaarlfgus@gmail.com>
2021-01-09 20:13:19 +0900

    Build Environment settings

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, {Component,Fragment} from 'react';
-import MainHeader from './Components/MainHeader';
-import MainBody from './Components/MainBody';
-import './App.css';
-import styled from 'styled-components';
+import React, { Component, Fragment } from "react";
+import MainHeader from "./Components/MainHeader";
+import MainBody from "./Components/MainBody";
+import "./App.css";
+import styled from "styled-components";
 
-class App extends Component{
-  render(){
+class App extends Component {
+  render() {
     return (
       <AppWrap>
-        <MainHeader/>
-        <MainBody/>  
+        <MainHeader />
+        <MainBody />
       </AppWrap>
-      );
+    );
   }
 }
 
 const AppWrap = styled.div`
-  font-size : 1.1rem;
-  width : 100vw;
+  font-size: 1.1rem;
+  width: 100vw;
   height: 100vh;
-  margin : 0 ;
-  display : flex;
-  flex-direction : column;
-  align-items : center;
-`
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default App;
