cf900ee5369aca7621a3b7d7e1e08ac88a448694
KilHyun-Kim <rlaarlfgus@gmail.com>
2021-01-10 14:35:55 +0900

    Connect with mongoDB and Node.js

--- backend/src/api/posts/posts.ctrl.js
+++ backend/src/api/posts/posts.ctrl.js
@@ -8,7 +8,7 @@ POST /api/posts
 { title, body } 
 */
 
-exports.write = (ctx) => {
+export const write = (ctx) => {
   // REST API의 Request Body 는 ctx.request.body 에서 조회할 수 있ㅇ므
   const { title, body } = ctx.request.body;
   postId += 1;
@@ -20,14 +20,14 @@ exports.write = (ctx) => {
 /** 포스트 목록 조회
  * GET /api/posts
  */
-exports.list = (ctx) => {
+export const list = (ctx) => {
   ctx.body = posts;
 };
 
 /** 특정 포스트 조회
  * GET api/posts/:id
  */
-exports.read = (ctx) => {
+export const read = (ctx) => {
   const { id } = ctx.params;
   // 주어진 id 값으로 포스트를 찾음
   // 파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 반환하거나
@@ -47,7 +47,7 @@ exports.read = (ctx) => {
 /** 특정 포스트 제거
  *  DELETE /api/posts/:id
  */
-exports.remove = (ctx) => {
+export const remove = (ctx) => {
   const { id } = ctx.params;
   // 해당 id를 가진 post가 몇 번재인지 확인함.
   const index = posts.findIndex((p) => p.id.toString() === id);
@@ -68,7 +68,7 @@ exports.remove = (ctx) => {
  * PUT /api/posts/:id
  * { title, body }
  */
-exports.replace = (ctx) => {
+export const replace = (ctx) => {
   // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용함.
   const { id } = ctx.params;
   // 해당 id를 가진 post가 몇 번재인지 확인
@@ -94,7 +94,7 @@ exports.replace = (ctx) => {
  * PATCH /api/posts/:id
  * { title, body }
  */
-exports.update = (ctx) => {
+export const update = (ctx) => {
   // PATCH 메서드는 주어진 필드만 교체합니다.
   const { id } = ctx.params;
   // 해당 id를 가진 post가 몇 번재인지 확인합니다.
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,4 @@
-/* Koa 의 장점 
-   미들웨어 사용 간편화
-   기본 형태
-   app.use((ctx, next) => {
-      next(); 
-   }) 
-   미들웨어 필요 시 사용
-   p608~616
-*/
+/* eslint-disable no-global-assign */
 
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-
-const api = require('./api');
-
-const app = new Koa();
-const router = new Router();
-
-// 라우터 설정
-router.use('/api', api.routes());
-
-app.use(bodyParser());
-
-app.use(router.routes()).use(router.allowedMethods());
-
-app.listen(4000, () => {
-  console.log('Lisenting');
-});
+require = require('esm')(module /*, options*/);
+module.exports = require('./main.js');
