35c2d3b74ffa8e4e3e08c4faac9578c828f2fca3
Lee Jin Woo <dlwlsdn201@naver.com>
2021-01-21 12:28:58 +0900

    {docs}: <Color palette 컴포넌트 문서 추가>
    <description>
    - 스타일링 시 사용할 색상을 쉽게 추출하기 위해 팔레트 용도로 사용될 문서 생성.
        : (path : src/lib/style/palette.js)
    
    <docs-#009>

---
+++ src/Components/auth/AuthForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import palette from '../../lib/styles/palette';
+import Button from '../common/Button';
+const AuthForm = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AuthForm;
