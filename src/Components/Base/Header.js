ef8cc8b3b7f6fa1d883e02fe9eb03541e4e6f87c
Lee Jin Woo <dlwlsdn201@naver.com>
2021-01-24 21:14:40 +0900

    {fix}: <nav menu style 수정 등>
    <description>
    -nav menu에 마우스 hover 시 background 색상 설정
    - nav menu의 Link 요소 width,height 수정(상위요소 li에 꽉 차게 각각 100%)
    
    <fix-#004>

--- src/Components/Base/Header.js
+++ src/Components/Base/Header.js
@@ -1,42 +1,42 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import Logo from '../../lib/images/logo.png';
-import {Link} from 'react-router-dom';
+import Logo from "../../lib/images/logo.png";
+import { Link } from "react-router-dom";
 
 const StyledHeaderContainer = styled.header`
   position: fixed;
   display: flex;
-  flex-flow : row wrap;
+  flex-flow: row wrap;
   justify-content: space-between;
-  width : 100%;
-  height : 3.5em;
-  border : 1px solid green;
+  width: 100%;
+  height: 3.5em;
+  border: 1px solid green;
   color: white;
   cursor: auto;
   z-index: 99;
   font-size: 1.5em;
 
-  &:hover{
-    background: rgba(66, 13, 136,0.4);
+  &:hover {
+    background: rgba(66, 13, 136, 0.5);
   }
-`
+`;
 
 const StyledLogoWrap = styled.div`
   /* border : 2px dashed red; */
-  width : 7.5rem;
+  width: 7.5rem;
   height: 100%;
-  min-width : 5rem;
-`
+  min-width: 5rem;
+`;
 
 const StyledNavWrap = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
   /* border : 2px dashed blue; */
-  width : 70%;
-  height : 100%;
-  padding : 0.2em 1rem;
-`
+  width: 70%;
+  height: 100%;
+  padding: 0.2em 1rem;
+`;
 
 const StyledNavMenu = styled.li`
   display: flex;
@@ -44,13 +44,18 @@ const StyledNavMenu = styled.li`
   justify-content: center;
   list-style: none;
   flex-grow: 1;
-  margin : 0 0.5rem;
-  padding : auto 100px;
+  margin: 0 0.5rem;
+  padding: auto 100px;
   /* border : 1px solid purple; */
-  width : 5em;
+  width: 5em;
   height: 100%;
-  font-size : 0.8em;
-`
+  font-size: 0.8em;
+  
+  a{height: 100%; width: 100%; padding: 1.5rem 0}
+  &:hover {
+    background: #420d88;
+  }
+`;
 
 class Header extends Component {
   render() {
@@ -58,25 +63,28 @@ class Header extends Component {
       <StyledHeaderContainer>
         <StyledLogoWrap>
           <Link to="/login">
-            <img src={Logo} alt="home"/>
+            <img src={Logo} alt="home" />
           </Link>
         </StyledLogoWrap>
         <StyledNavWrap>
-            <StyledNavMenu>
-              <Link to="/About">About</Link>
-            </StyledNavMenu>
-            <StyledNavMenu>
+          <StyledNavMenu>
+            <Link to="/About">About</Link>
+          </StyledNavMenu>
+          <StyledNavMenu>
             <Link to="/Co-Study">Co-Study</Link>
-            </StyledNavMenu><StyledNavMenu>
-              <Link to="/Co-Project">Co-Project</Link>
-            </StyledNavMenu><StyledNavMenu>
-              <Link to="/Q&A">Q&A</Link>
-            </StyledNavMenu><StyledNavMenu>
-              <Link to="/MyPage">MyPage</Link>
-            </StyledNavMenu>
+          </StyledNavMenu>
+          <StyledNavMenu>
+            <Link to="/Co-Project">Co-Project</Link>
+          </StyledNavMenu>
+          <StyledNavMenu>
+            <Link to="/Q&A">Q&A</Link>
+          </StyledNavMenu>
+          <StyledNavMenu>
+            <Link to="/MyPage">MyPage</Link>
+          </StyledNavMenu>
         </StyledNavWrap>
       </StyledHeaderContainer>
-      );
+    );
   }
 }
 
