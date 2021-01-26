765ecc083f768012df4e67319a7dbd153ec1ac0b
LeeJinWoo <dlwlsdn201@naver.com>
2021-01-26 15:04:04 +0900

    {feature}: <nav 컴포넌트 내부 state 추가 등>
    <description>
    - nav 컴포넌트 내부 menu의 text 값을 state 값으로 변환함.
    - 디바이스 크기별 header 영역 레이아웃 영역 수정.
    
    <issue-number>
    fix-#1

--- src/Components/Base/Header.js
+++ src/Components/Base/Header.js
@@ -1,9 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Logo from "../../lib/images/logo.png";
 import { Link } from "react-router-dom";
-
-
 
 const StyledHeaderContainer = styled.header`
   position: fixed;
@@ -13,42 +11,43 @@ const StyledHeaderContainer = styled.header`
   justify-content: space-between;
   width: 100vw;
   height: 14vh;
-  max-height: 14vh;
+  /* max-height: 14vh; */
   /* border: 1px solid green; */
   color: white;
   cursor: auto;
   z-index: 99;
   font-size: 3vh;
-  background: rgba(0,0,0,0.7);
+  background: rgba(0, 0, 0, 0.7);
 
   &:hover {
     background: rgba(66, 13, 136, 0.5);
   }
 
-
-  @media(min-width: 320px) and (max-width: 768px){
-    height: 10vh;
-    background: blue;
+  @media (min-width: 768px) and (max-width: 769px) {
+    background: orange;
+    height: 13vh;
   }
 
+  @media (min-width: 320px) and (max-width: 767px) {
+    height: 9vh;
+    background: blue;
+  }
 `;
 
 const StyledLogoWrap = styled.div`
   /* border : 2px dashed red; */
-  width: 12vw;
+  width: 14vw;
   height: 100%;
 
-  @media(min-width: 320px) and (max-width: 768px){
-    width: 24vw;
+  @media (min-width: 320px) and (max-width: 768px) {
+    width: 22vw;
     height: 100%;
   }
 
-  @media(min-width: 768px) and (max-width: 768px){
-    width: 14vw;
+  @media (min-width: 768px) and (max-width: 768px) {
+    width: 18vw;
     height: 100%;
   }
-
-
 `;
 
 const StyledNavWrap = styled.nav`
@@ -59,17 +58,16 @@ const StyledNavWrap = styled.nav`
   width: 78%;
   height: 100%;
 
-  @media(min-width: 320px) and (max-width: 768px){
+  @media (min-width: 320px) and (max-width: 768px) {
     width: 76vw;
     height: 100%;
-    font-size: 1.8vh;
+    font-size: 2vh;
   }
 
-  @media(min-width: 768px) and (max-width: 768px){
-    width: 86vw;
+  @media (min-width: 768px) and (max-width: 768px) {
+    width: 80vw;
     font-size: 2.5vh;
   }
-
 `;
 
 const StyledNavMenu = styled.li`
@@ -82,39 +80,39 @@ const StyledNavMenu = styled.li`
   padding: 0;
   width: 100%;
   height: 100%;
-  
+
   &:hover {
     background: #420d88;
   }
-  `;
+`;
 
 const Header = () => {
+  const [menu, setMenu] = useState([
+    { id: 1, title: "About" },
+    { id: 2, title: "Co-Study" },
+    { id: 3, title: "Co-Project" },
+    { id: 4, title: "Q&A" },
+    { id: 5, title: "MyPage" },
+  ]);
+
+  const menuLinkList = menu.map((MENU) => {
     return (
-      <StyledHeaderContainer>
-        <StyledLogoWrap>
-          <Link to="/login">
-            <img src={Logo} alt="home" />
-          </Link>
-        </StyledLogoWrap>
-        <StyledNavWrap>
-          <StyledNavMenu>
-            <Link to="/About">About</Link>
-          </StyledNavMenu>
-          <StyledNavMenu>
-            <Link to="/Co-Study">Co-Study</Link>
-          </StyledNavMenu>
-          <StyledNavMenu>
-            <Link to="/Co-Project">Co-Project</Link>
-          </StyledNavMenu>
-          <StyledNavMenu>
-            <Link to="/Q&A">Q&A</Link>
-          </StyledNavMenu>
-          <StyledNavMenu>
-            <Link to="/MyPage">MyPage</Link>
-          </StyledNavMenu>
-        </StyledNavWrap>
-      </StyledHeaderContainer>
+      <StyledNavMenu>
+        <Link to={`/ ${MENU.title}`}>{MENU.title}</Link>
+      </StyledNavMenu>
     );
-}
+  });
+
+  return (
+    <StyledHeaderContainer>
+      <StyledLogoWrap>
+        <Link to="/login">
+          <img src={Logo} alt="home" />
+        </Link>
+      </StyledLogoWrap>
+      <StyledNavWrap>{menuLinkList}</StyledNavWrap>
+    </StyledHeaderContainer>
+  );
+};
 
 export default Header;
